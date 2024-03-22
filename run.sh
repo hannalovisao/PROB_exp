cca10df55ffe4e5f8823b24f6917042305d4eebc
luckylukez <lukas.g.johansson@gmail.com>
2024-03-22 14:04:00 +0100

    Updated path to config files

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/M_OWOD_BENCHMARK.sh
+#GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/M_OWOD_BENCHMARK.sh
+
+GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/ZOD_OWOD_BENCHMARK.sh
