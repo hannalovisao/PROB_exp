9f339be81e82723659fe34af4ec0fd3566f47ed2
luckylukez <lukas.g.johansson@gmail.com>
2024-03-28 13:48:01 +0100

    removed the difficult attribute since we dont have that in our annotations

--- datasets/open_world_eval.py
+++ datasets/open_world_eval.py
@@ -274,7 +274,8 @@ def parse_rec(filename, known_classes):
         if cls_name not in known_classes:
             cls_name = 'unknown'
         obj_struct['name'] = cls_name
-        obj_struct['difficult'] = int(obj.find('difficult').text)
+        #obj_struct['difficult'] = int(obj.find('difficult').text)
+        obj_struct['difficult'] = 0 #We don't have difficult attribute in our dataset
         bbox = obj.find('bndbox')
         obj_struct['bbox'] = [int(bbox.find('xmin').text),
                               int(bbox.find('ymin').text),
