31daf7c20a6dc641faec2e7a8e61c3fd1a99f471
luckylukez <lukas.g.johansson@gmail.com>
2024-04-18 09:38:24 +0200

    First working benchmark for zod

--- configs/ZOD_OWOD_BENCHMARK.sh
+++ configs/ZOD_OWOD_BENCHMARK.sh
@@ -4,9 +4,10 @@ echo running training of prob-detr, M-OWODB dataset
 
 set -x
 
-EXP_DIR=exps/ZOD/PROB
+EXP_DIR=exps/ZOD_clear/PROB
 PY_ARGS=${@:1}
 WANDB_NAME=PROB_ZOD
+DATASET="ZOD"
 
 #!/bin/bash
 
@@ -26,76 +27,79 @@ echo "Using $PYTHON_CMD"
 $PYTHON_CMD -c "print('Hello, World!')"
 
 $PYTHON_CMD -u main_open_world.py \
-    --output_dir "${EXP_DIR}/t1" --dataset ZOD --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 14\
-    --train_set 'zod_train' --test_set 'zod_all_task_test' --epochs 10\
+    --output_dir "${EXP_DIR}/t1" --dataset $DATASET --PREV_INTRODUCED_CLS 0 --CUR_INTRODUCED_CLS 4\
+    --train_set 'zod_cropped_t1_train_clear' --test_set 'zod_cropped_test_clear' --epochs 41\
     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
     --wandb_name "${WANDB_NAME}_t1" --exemplar_replay_selection --exemplar_replay_max_length 850\
     --exemplar_replay_dir ${WANDB_NAME} --exemplar_replay_cur_file "learned_zod_t1_ft.txt"\
     ${PY_ARGS}
     
 
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t2" --dataset TOWOD --PREV_INTRODUCED_CLS 20 --CUR_INTRODUCED_CLS 20\
-#     --train_set 'owod_t2_train' --test_set 'owod_all_task_test' --epochs 51\
-#     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3 --freeze_prob_model\
-#     --wandb_name "${WANDB_NAME}_t2"\
-#     --exemplar_replay_selection --exemplar_replay_max_length 1743 --exemplar_replay_dir ${WANDB_NAME}\
-#     --exemplar_replay_prev_file "learned_owod_t1_ft.txt" --exemplar_replay_cur_file "learned_owod_t2_ft.txt"\
-#     --pretrain "${EXP_DIR}/t1/checkpoint0040.pth" --lr 2e-5\
-#     ${PY_ARGS}
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+    --output_dir "${EXP_DIR}/t2" --dataset $DATASET --PREV_INTRODUCED_CLS 4 --CUR_INTRODUCED_CLS 2\
+    --train_set 'zod_cropped_t2_train_clear' --test_set 'zod_cropped_test_clear' --epochs 51\
+    --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3 --freeze_prob_model\
+    --wandb_name "${WANDB_NAME}_t2"\
+    --exemplar_replay_selection --exemplar_replay_max_length 1743 --exemplar_replay_dir ${WANDB_NAME}\
+    --exemplar_replay_prev_file "learned_zod_t1_ft.txt" --exemplar_replay_cur_file "learned_zod_t2_ft.txt"\
+    --pretrain "${EXP_DIR}/t1/checkpoint0040.pth" --lr 2e-5\
+     ${PY_ARGS}
     
 
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t2_ft" --dataset TOWOD --PREV_INTRODUCED_CLS 20 --CUR_INTRODUCED_CLS 20 \
-#     --train_set "${WANDB_NAME}/learned_owod_t2_ft" --test_set 'owod_all_task_test' --epochs 111 --lr_drop 40\
-#     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-#     --wandb_name "${WANDB_NAME}_t2_ft"\
-#     --pretrain "${EXP_DIR}/t2/checkpoint0050.pth"\
-#     ${PY_ARGS}
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+     --output_dir "${EXP_DIR}/t2_ft" --dataset $DATASET --PREV_INTRODUCED_CLS 4 --CUR_INTRODUCED_CLS 2 \
+     --train_set "${WANDB_NAME}/learned_zod_t2_ft" --test_set 'zod_cropped_test_clear' --epochs 111 --lr_drop 40\
+     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
+     --wandb_name "${WANDB_NAME}_t2_ft"\
+     --pretrain "${EXP_DIR}/t2/checkpoint0050.pth"\
+     ${PY_ARGS}
+    
+
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+     --output_dir "${EXP_DIR}/t3" --dataset $DATASET --PREV_INTRODUCED_CLS 6 --CUR_INTRODUCED_CLS 4\
+     --train_set 'zod_cropped_t3_train_clear' --test_set 'zod_cropped_test_clear' --epochs 121\
+     --model_type 'prob' --obj_loss_coef 8e-4 --freeze_prob_model --obj_temp 1.3\
+     --wandb_name "${WANDB_NAME}_t3"\
+     --exemplar_replay_selection --exemplar_replay_max_length 2361 --exemplar_replay_dir ${WANDB_NAME}\
+     --exemplar_replay_prev_file "learned_zod_t2_ft.txt" --exemplar_replay_cur_file "learned_zod_t3_ft.txt"\
+     --pretrain "${EXP_DIR}/t2_ft/checkpoint0110.pth" --lr 2e-5 \
+     ${PY_ARGS}
+    
+
+ 
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+     --output_dir "${EXP_DIR}/t3_ft" --dataset $DATASET --PREV_INTRODUCED_CLS 6 --CUR_INTRODUCED_CLS 4 \
+     --train_set "${WANDB_NAME}/learned_zod_t3_ft" --test_set 'zod_cropped_test_clear' --epochs 181 --lr_drop 35\
+     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
+     --wandb_name "${WANDB_NAME}_t3_ft"\
+     --pretrain "${EXP_DIR}/t3/checkpoint0120.pth"\
+     ${PY_ARGS}
+
+
+
+    
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+    --output_dir "${EXP_DIR}/t4" --dataset $DATASET --PREV_INTRODUCED_CLS 10 --CUR_INTRODUCED_CLS 4\
+    --train_set 'zod_cropped_t4_train_clear' --test_set 'zod_cropped_test_clear' --epochs 191 \
+    --model_type 'prob' --obj_loss_coef 8e-4 --freeze_prob_model --obj_temp 1.3\
+    --wandb_name "${WANDB_NAME}_t4"\
+    --exemplar_replay_selection --exemplar_replay_max_length 2749 --exemplar_replay_dir ${WANDB_NAME}\
+    --exemplar_replay_prev_file "learned_zod_t3_ft.txt" --exemplar_replay_cur_file "learned_zod_t4_ft.txt"\
+    --num_inst_per_class 40\
+    --pretrain "${EXP_DIR}/t3_ft/checkpoint0180.pth" --lr 2e-5\
+    ${PY_ARGS}
     
     
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t3" --dataset TOWOD --PREV_INTRODUCED_CLS 40 --CUR_INTRODUCED_CLS 20\
-#     --train_set 'owod_t3_train' --test_set 'owod_all_task_test' --epochs 121\
-#     --model_type 'prob' --obj_loss_coef 8e-4 --freeze_prob_model --obj_temp 1.3\
-#     --wandb_name "${WANDB_NAME}_t3"\
-#     --exemplar_replay_selection --exemplar_replay_max_length 2361 --exemplar_replay_dir ${WANDB_NAME}\
-#     --exemplar_replay_prev_file "learned_owod_t2_ft.txt" --exemplar_replay_cur_file "learned_owod_t3_ft.txt"\
-#     --pretrain "${EXP_DIR}/t2_ft/checkpoint0110.pth" --lr 2e-5 \
-#     ${PY_ARGS}
-    
-    
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t3_ft" --dataset TOWOD --PREV_INTRODUCED_CLS 40 --CUR_INTRODUCED_CLS 20 \
-#     --train_set "${WANDB_NAME}/learned_owod_t3_ft" --test_set 'owod_all_task_test' --epochs 181 --lr_drop 35\
-#     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-#     --wandb_name "${WANDB_NAME}_t3_ft"\
-#     --pretrain "${EXP_DIR}/t3/checkpoint0120.pth"\
-#     ${PY_ARGS}
-    
-    
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t4" --dataset TOWOD --PREV_INTRODUCED_CLS 60 --CUR_INTRODUCED_CLS 20\
-#     --train_set 'owod_t4_train' --test_set 'owod_all_task_test' --epochs 191 \
-#     --model_type 'prob' --obj_loss_coef 8e-4 --freeze_prob_model --obj_temp 1.3\
-#     --wandb_name "${WANDB_NAME}_t4"\
-#     --exemplar_replay_selection --exemplar_replay_max_length 2749 --exemplar_replay_dir ${WANDB_NAME}\
-#     --exemplar_replay_prev_file "learned_owod_t3_ft.txt" --exemplar_replay_cur_file "learned_owod_t4_ft.txt"\
-#     --num_inst_per_class 40\
-#     --pretrain "${EXP_DIR}/t3_ft/checkpoint0180.pth" --lr 2e-5\
-#     ${PY_ARGS}
-    
-    
-# PY_ARGS=${@:1}
-# python -u main_open_world.py \
-#     --output_dir "${EXP_DIR}/t4_ft" --dataset TOWOD --PREV_INTRODUCED_CLS 60 --CUR_INTRODUCED_CLS 20\
-#     --train_set "${WANDB_NAME}/learned_owod_t4_ft" --test_set 'owod_all_task_test' --epochs 261 --lr_drop 50\
-#     --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
-#     --wandb_name "${WANDB_NAME}_t4_ft"\
-#     --pretrain "${EXP_DIR}/t4/checkpoint0190.pth" \
-#     ${PY_ARGS}
+PY_ARGS=${@:1}
+$PYTHON_CMD -u main_open_world.py \
+    --output_dir "${EXP_DIR}/t4_ft" --dataset $DATASET --PREV_INTRODUCED_CLS 10 --CUR_INTRODUCED_CLS 4\
+    --train_set "${WANDB_NAME}/learned_zod_t4_ft" --test_set 'zod_cropped_test_clear' --epochs 261 --lr_drop 50\
+    --model_type 'prob' --obj_loss_coef 8e-4 --obj_temp 1.3\
+    --wandb_name "${WANDB_NAME}_t4_ft"\
+    --pretrain "${EXP_DIR}/t4/checkpoint0190.pth" \
+    ${PY_ARGS}
